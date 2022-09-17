731550c8488b7e1fe36cf5a9fcbf59a1fc29ff2d
Igor Sily <igorsily2@gmail.com>
2022-09-17 12:16:13 -0300

    updating UPC and adjust tests

--- src/main/java/com/igorsily/msscbeerservice/domain/Beer.java
+++ src/main/java/com/igorsily/msscbeerservice/domain/Beer.java
@@ -44,7 +44,7 @@ public class Beer {
     private BeerStyleEnum beerStyle;
 
     @Column(unique = true)
-    private Long upc;
+    private String upc;
 
     private BigDecimal price;
 
--- src/test/java/com/igorsily/msscbeerservice/controller/BeerControllerTest.java
+++ src/test/java/com/igorsily/msscbeerservice/controller/BeerControllerTest.java
@@ -1,11 +1,14 @@
 package com.igorsily.msscbeerservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.igorsily.msscbeerservice.bootstrap.BeerLoader;
 import com.igorsily.msscbeerservice.model.BeerDTO;
 import com.igorsily.msscbeerservice.model.BeerStyleEnum;
+import com.igorsily.msscbeerservice.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,10 +16,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
+
 
     @Autowired
     MockMvc mockMvc;
@@ -24,8 +30,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerService beerService;
+
     @Test
     void getBeerById() throws Exception {
+
+        given(this.beerService.getById(any())).willReturn(getValidBeerDTO());
 
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/v1/beers/" + UUID.randomUUID())
@@ -39,6 +50,8 @@ class BeerControllerTest {
         BeerDTO beerDTO = getValidBeerDTO();
 
         String beerJson = objectMapper.writeValueAsString(beerDTO);
+
+        given(this.beerService.saveBeer(any())).willReturn(getValidBeerDTO());
 
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/v1/beers/")
@@ -54,6 +67,7 @@ class BeerControllerTest {
 
         String beerJson = objectMapper.writeValueAsString(beerDTO);
 
+        given(this.beerService.updateBeer(any(), any())).willReturn(getValidBeerDTO());
 
         mockMvc.perform(MockMvcRequestBuilders
                         .put("/api/v1/beers/" + UUID.randomUUID())
@@ -67,7 +81,7 @@ class BeerControllerTest {
                 .beerName("MY BEER")
                 .beerStyle(BeerStyleEnum.IPA)
                 .price(new BigDecimal("11.99"))
-                .upc(124512645611L)
+                .upc(BeerLoader.BEER_1_UPC  )
                 .build();
     }
 }
